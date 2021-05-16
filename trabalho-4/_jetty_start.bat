47708c20f705e2278076f6f017824f8fc4912295
Bruno Costa <bruno.costa@strix.pt>
2021-05-16 17:57:40 +0100

    Aula prática 4

---
+++ trabalho-4/_jetty_start.bat
@@ -0,0 +1,13 @@
+#PTS
+
+
+set jetty=".\..\jetty-distribution-9.4.8.v20171121"
+
+cd %jetty%\
+
+java -jar start.jar jetty.http.port=8280
+
+#PTS
+# depois de iniciar o Jetty (servidor http) pode testar o RDF4J (ex Sesame), para isso:
+# 1) inicie o Browser
+# 2) aponte o Browser para: http://localhost:8280/rdf4j-workbench
